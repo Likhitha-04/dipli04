744bafa5d8576d3965faaf2fc9c7cbd37d52033e
chand <chand@MSI>
2022-11-29 21:09:03 +0530

    likhitha

---
+++ src/main/java/CSEB/likhi/App.java
@@ -0,0 +1,14 @@
+package CSEB.likhi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Deveops Lab!" );
+    }
+}
